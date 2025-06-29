208e604c2fd12da5e1a09c91e84e662cbc270574
inamnurulf <aam.inam7310@gmail.com>
2025-06-29 18:16:57 +0700

    feat: update database schema by adding users and refresh_tokens tables, and reordering device_sensors table

--- src/config/postgre.js
+++ src/config/postgre.js
@@ -38,18 +38,6 @@ async function checkAndCreateTables() {
             CONSTRAINT articles_pkey PRIMARY KEY (id)
         );
       `,
-      device_sensors: `
-        CREATE TABLE public.device_sensors (
-            id uuid DEFAULT gen_random_uuid() NOT NULL,
-            device_id uuid NOT NULL,
-            sensor_type_id uuid NOT NULL,
-            created_at timestamptz DEFAULT now() NULL,
-            CONSTRAINT device_sensors_device_id_sensor_type_id_key UNIQUE (device_id, sensor_type_id),
-            CONSTRAINT device_sensors_pkey PRIMARY KEY (id),
-            CONSTRAINT device_sensors_device_id_fkey FOREIGN KEY (device_id) REFERENCES public.onboarding_devices(id) ON DELETE CASCADE,
-            CONSTRAINT device_sensors_sensor_type_id_fkey FOREIGN KEY (sensor_type_id) REFERENCES public.sensor_types(id) ON DELETE CASCADE
-        );
-      `,
       onboarding_devices: `
         CREATE TABLE public.onboarding_devices (
             id uuid DEFAULT gen_random_uuid() NOT NULL,
@@ -64,6 +52,43 @@ async function checkAndCreateTables() {
             CONSTRAINT onboarding_devices_status_check CHECK ((status = ANY (ARRAY['active'::text, 'inactive'::text])))
         );
       `,
+      users: `
+        CREATE TABLE public.users (
+            id uuid DEFAULT gen_random_uuid() NOT NULL,
+            "name" varchar(255) NOT NULL,
+            email varchar(255) NOT NULL,
+            password_hash varchar(255) NOT NULL,
+            "role" varchar(50) DEFAULT 'user'::character varying NULL,
+            is_active bool DEFAULT false NOT NULL,
+            created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+            verification_code varchar NULL,
+            CONSTRAINT users_email_key UNIQUE (email),
+            CONSTRAINT users_pkey PRIMARY KEY (id)
+        );
+      `,
+            sensor_types: `
+        CREATE TABLE public.sensor_types (
+            id uuid DEFAULT gen_random_uuid() NOT NULL,
+            "name" text NOT NULL,
+            unit text NULL,
+            description text NULL,
+            CONSTRAINT sensor_types_name_key UNIQUE (name),
+            CONSTRAINT sensor_types_pkey PRIMARY KEY (id)
+        );
+      `,
+      device_sensors: `
+        CREATE TABLE public.device_sensors (
+            id uuid DEFAULT gen_random_uuid() NOT NULL,
+            device_id uuid NOT NULL,
+            sensor_type_id uuid NOT NULL,
+            created_at timestamptz DEFAULT now() NULL,
+            CONSTRAINT device_sensors_device_id_sensor_type_id_key UNIQUE (device_id, sensor_type_id),
+            CONSTRAINT device_sensors_pkey PRIMARY KEY (id),
+            CONSTRAINT device_sensors_device_id_fkey FOREIGN KEY (device_id) REFERENCES public.onboarding_devices(id) ON DELETE CASCADE,
+            CONSTRAINT device_sensors_sensor_type_id_fkey FOREIGN KEY (sensor_type_id) REFERENCES public.sensor_types(id) ON DELETE CASCADE
+        );
+      `,
       refresh_tokens: `
         CREATE TABLE public.refresh_tokens (
             id uuid DEFAULT gen_random_uuid() NOT NULL,
@@ -76,16 +101,6 @@ async function checkAndCreateTables() {
             CONSTRAINT refresh_tokens_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE
         );
       `,
-      sensor_types: `
-        CREATE TABLE public.sensor_types (
-            id uuid DEFAULT gen_random_uuid() NOT NULL,
-            "name" text NOT NULL,
-            unit text NULL,
-            description text NULL,
-            CONSTRAINT sensor_types_name_key UNIQUE (name),
-            CONSTRAINT sensor_types_pkey PRIMARY KEY (id)
-        );
-      `,
       server_errors: `
         CREATE TABLE public.server_errors (
             id serial4 NOT NULL,
@@ -96,21 +111,6 @@ async function checkAndCreateTables() {
             request_body jsonb NULL,
             created_at timestamp DEFAULT now() NULL,
             CONSTRAINT server_errors_pkey PRIMARY KEY (id)
-        );
-      `,
-      users: `
-        CREATE TABLE public.users (
-            id uuid DEFAULT gen_random_uuid() NOT NULL,
-            "name" varchar(255) NOT NULL,
-            email varchar(255) NOT NULL,
-            password_hash varchar(255) NOT NULL,
-            "role" varchar(50) DEFAULT 'user'::character varying NULL,
-            is_active bool DEFAULT false NOT NULL,
-            created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-            verification_code varchar STORAGE MAIN NULL,
-            CONSTRAINT users_email_key UNIQUE (email),
-            CONSTRAINT users_pkey PRIMARY KEY (id)
         );
       `,
       warnings: `
